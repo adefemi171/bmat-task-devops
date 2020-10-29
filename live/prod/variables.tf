705960c12d5d922c41542d20cacdad93f1603751
adefemi171 <adefemi171@gmail.com>
2020-10-29 13:03:22 -0400

    Created infrastructure for Prod

---
+++ live/prod/variables.tf
@@ -0,0 +1,32 @@
+variable "region_main" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = "stage-vpc"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  default     = "0.0.0.0/0"
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
