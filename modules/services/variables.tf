8d94bcf6c4963851c321fb13c75962ca2ccb155a
adefemi171 <adefemi171@gmail.com>
2020-10-29 14:01:07 -0400

    Created ASG module

--- modules/services/variables.tf
+++ modules/services/variables.tf
@@ -3,6 +3,17 @@ variable "region_main" {
   default = "us-east-1"
 }
 
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = ""
+}
+
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  default     = "0.0.0.0/0"
+}
+
 variable "lcname" {
   description = "Name to be used on all the resources as identifier"
   default     = ""
